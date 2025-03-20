48c4d904f3fec2ab91714172bde8da1063e0a759
giaki <grigoriosvousios@gmail.com>
2025-03-20 09:37:08 +0100

    base for site is made

---
+++ indexBuild.js
@@ -0,0 +1,13 @@
+
+
+const overCentral = document.querySelector("body")
+
+overCentral.innerHTML = `
+<div id="wrapper">
+<header id="centralHeader" class="primaris">hed</header><!--centralHeader-->
+<main></main class="primaris">mai<!--main-->
+<footer id="centralFooter" class="primaris">foot</footer><!--CentralFooter-->
+</div><!--wrapper-->
+`
+document.querySelector("body").append(overCentral);
+
