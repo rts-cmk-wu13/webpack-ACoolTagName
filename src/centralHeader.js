1805e336a78fbb414b2b8cad12f7ed140041da0c
giaki <grigoriosvousios@gmail.com>
2025-03-20 10:12:39 +0100

    I do somthing I think

---
+++ src/centralHeader.js
@@ -0,0 +1,17 @@
+
+
+const headerMain = document.getElementById("centralHeader")
+
+headerMain.innerHTML = `
+<article>
+    <nav>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </nav>
+</article>
+`
+document.getElementById("centralHeader").append(headerMain);
+
---
+++ src/indexBuild.js
@@ -0,0 +1,13 @@
+
+
+const overCentral = document.querySelector("body")
+
+overCentral.innerHTML = `
+<div id="wrapper">
+<header id="centralHeader" class="primaris"></header><!--centralHeader-->
+<main></main class="primaris"><!--main-->
+<footer id="centralFooter" class="primaris"></footer><!--CentralFooter-->
+</div><!--wrapper-->
+`
+document.querySelector("body").append(overCentral);
+
